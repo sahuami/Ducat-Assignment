1991e3d449b86b5f9d67cf1536a2b085bf63d461
Amit Sahu <sahuami2000@gmail.com>
2024-07-18 09:26:47 +0530

    add new

---
+++ JAVASCRIPT/ASSIGNMENT_1_Introduction_To_Javascript_and_Variables_Data_Type/index.js
@@ -0,0 +1,30 @@
+// PROBLEM 1:
+console.log("Ducat An IT Institute\n","A Transformation in Education")
+
+
+// PROBLEM 2:
+var Name = "amit"
+console.log(Name)
+var father_name = "ganga dayal"
+console.log(father_name)
+var Mother_name ="Rita"
+console.log(Mother_name)
+
+
+// PROBLEM 3:
+var myName = "Amit Sahu";
+var myAge = 24;
+console.log(myName , myAge, "\n" , typeof(myName), typeof(myAge) )
+
+
+// PROBLEM 4:
+console.log("-------------------------------------------------------------------")
+console.log("name        School        Section        Grade        Marks"    )
+console.log("-------------------------------------------------------------------")
+console.log("Amit        GBSSS         A              A            90")
+console.log("-------------------------------------------------------------------")
+
+
+
+
+
