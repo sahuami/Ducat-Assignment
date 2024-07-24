47d2d7f0141fded2a56e698c2aacddfe3b296295
Amit Sahu <sahuami2000@gmail.com>
2024-07-24 13:59:34 +0530

    add

---
+++ JAVASCRIPT/ASSIGNMENT_4_Conditional/index.js
@@ -0,0 +1,51 @@
+
+// QUESTION 1:
+// let a = 24 , b =24
+// if(a>b){
+//     console.log("a is larger")
+// }
+// else if(a==b){
+//     console.log("both are equal")
+// }
+// else{
+//     console.log("b is larger")
+// }
+
+
+//QUESTION 2:
+// let age = 18;
+// if (age >= 18) {
+//     console.log("you are adult")
+// } else {
+//     console.log("you are minor")
+// }
+
+
+
+//QUESTION 3:
+// let num = 31
+// if (num % 5 == 0) {
+//     console.log(num + " is divisible by 5")
+// } else {
+//     console.log(num + " is not divisible by 5")
+
+// }
+
+//QUESTION 4:
+// let num = 12
+// if (num % 2 == 0) {
+//     console.log("number is even")
+// } else {
+//     console.log("number is odd")
+
+// }
+
+
+// QUESTION 5:
+// let number = -4
+// if(number>0){
+//     console.log("number is positive")
+// }else{
+//     console.log("number is negative")
+    
+// }
